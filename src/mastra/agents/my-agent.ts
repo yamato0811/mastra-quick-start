587503f34843898da2142fda1b81413189a17ab1
yamato0811 <yama8to11@gmail.com>
2025-05-22 18:52:26 +0900

    first commit

---
+++ src/mastra/agents/my-agent.ts
@@ -0,0 +1,17 @@
+import { openai } from "@ai-sdk/openai";
+import { Agent } from "@mastra/core/agent";
+import { Memory } from "@mastra/memory";
+import { LibSQLStore } from "@mastra/libsql";
+
+export const myAgent = new Agent({
+  name: "My Agent",
+  instructions: `
+      You are a helpful assistant.
+`,
+  model: openai("gpt-4o-mini"),
+  memory: new Memory({
+    storage: new LibSQLStore({
+      url: "file:../mastra.db", // path is relative to the .mastra/output directory
+    }),
+  }),
+});
---
+++ src/mastra/index.ts
@@ -0,0 +1,30 @@
+import { Mastra } from "@mastra/core/mastra";
+import { PinoLogger } from "@mastra/loggers";
+import { LibSQLStore } from "@mastra/libsql";
+import { LangfuseExporter } from "langfuse-vercel";
+
+import { myAgent } from "./agents/my-agent";
+
+export const mastra = new Mastra({
+  agents: { myAgent },
+  storage: new LibSQLStore({
+    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
+    url: ":memory:",
+  }),
+  logger: new PinoLogger({
+    name: "Mastra",
+    level: "info",
+  }),
+  telemetry: {
+    serviceName: "ai", // this must be set to "ai" so that the LangfuseExporter thinks it's an AI SDK trace
+    enabled: true,
+    export: {
+      type: "custom",
+      exporter: new LangfuseExporter({
+        publicKey: process.env.LANGFUSE_PUBLIC_KEY,
+        secretKey: process.env.LANGFUSE_SECRET_KEY,
+        baseUrl: process.env.LANGFUSE_BASEURL,
+      }),
+    },
+  },
+});
